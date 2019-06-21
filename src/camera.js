69b69df3b5da175dfe15ba7ce6cedb7ccf4a7943
hookex <cookmycode@gmail.com>
2019-06-21 16:49:15 +0800

    ...

--- src/camera.js
+++ src/camera.js
@@ -4,8 +4,8 @@ export function initCamera() {
     const fov = 45;
     const aspect = 2;  // the canvas default
     const near = 0.1;
-    const far = 20000;
+    const far = 40000;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(-1000, 2000, 4400);
+    camera.position.set(-800, 1600, 4000);
     return camera
 }
